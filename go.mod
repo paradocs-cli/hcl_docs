f19eeabcac343df350870bea2ead18378ed48f58
John Hession <johhess@cdw.com>
2022-03-26 11:54:00 -0700

    Building out code for parsing hcl

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/paradocs-cli/hcl_docs
 
 go 1.18
+
+require github.com/hashicorp/hcl v1.0.0
--- hcl_parse.go
+++ hcl_parse.go
@@ -1,6 +1,41 @@
 package hcl_docs
 
-func ParseHclBlocks()     {}
+import (
+	"fmt"
+	"github.com/hashicorp/hcl"
+	"github.com/hashicorp/hcl/hcl/ast"
+	"strconv"
+)
+
+type CommentData struct {
+	Filename string
+	Line     string
+	Text     string
+}
+
+//ParseHclBlocks takes an input of string and parses HCL blocks and comments
+func ParseHclBlocks(s string) (ast.File, error) {
+	parse, err := hcl.Parse(s)
+	if err != nil {
+		return *parse, fmt.Errorf(err.Error())
+	}
+	return *parse, nil
+}
+
+//ReadBlockComments takes an input of type ast.File and returns data about comments in the present hcl blocks 
+func ReadBlockComments(a ast.File) ([]CommentData, error) {
+	var comms []CommentData
+	for _, v := range a.Comments {
+		var c CommentData
+		for _, z := range v.List {
+			c.Filename = z.Pos().Filename
+			c.Line = strconv.Itoa(z.Pos().Line)
+			c.Text = z.Text
+			comms = append(comms, c)
+		}
+	}
+	return comms, nil
+}
 func ParseVarBlocks()     {}
 func ParseCommentBlocks() {}
 func ParseVarDefsBlocks() {}
